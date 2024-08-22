6897cc5f65f724a9eabd06a0cb943ef6dc29859d
yurafulmes <32300365+yurafulmes@users.noreply.github.com>
2024-08-22 17:51:40 +0300

    Update farmFluxDust.lua

--- farmFluxDust.lua
+++ farmFluxDust.lua
@@ -20,37 +20,4 @@ while true do
             handleObsidian()
         end
     end
-
-    -- Turn to the left, check and dig
-    turtle.turnLeft()
-    if turtle.detect() then
-        local success, data = turtle.inspect()
-        if success and data.name == "minecraft:obsidian" then
-            turtle.dig()
-            handleObsidian()
-        end
-    end
-
-    -- Turn to the left, check and dig (now facing back)
-    turtle.turnLeft()
-    if turtle.detect() then
-        local success, data = turtle.inspect()
-        if success and data.name == "minecraft:obsidian" then
-            turtle.dig()
-            handleObsidian()
-        end
-    end
-
-    -- Turn to the left, check and dig (now facing right)
-    turtle.turnLeft()
-    if turtle.detect() then
-        local success, data = turtle.inspect()
-        if success and data.name == "minecraft:obsidian" then
-            turtle.dig()
-            handleObsidian()
-        end
-    end
-
-    -- Turn back to the original direction
-    turtle.turnLeft()
 end
